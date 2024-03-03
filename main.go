6484895366dbaa452c5a003e6cdaf39e3847ef29
Jesus Rodriguez <jesusrodriguez7@comcast.net>
2024-03-03 11:28:08 -0700

    added some comments and updated print statements to use Printf

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"fmt" //imports format package, used for things like Print, Println, etc
 )
 
 func main() {
@@ -9,8 +9,9 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = 50
 
-	fmt.Println("Welcome to our", conferenceName, "booking application")
-	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
+	//fmt.Println("Welcome to our", conferenceName, "booking application") //using normal println
+	fmt.Printf("Welcome to our %v booking application\n", conferenceName) //using Printf with a placehoulder %v
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
 }
